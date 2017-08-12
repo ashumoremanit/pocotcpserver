963788f90744f4350e31934da075435d7566f330
Ashutosh More <ashumoremanit@gmail.com>
2017-08-12 22:41:14 +0530

    Update TCPServer.cpp

--- cppsourcefile/TCPServer.cpp
+++ cppsourcefile/TCPServer.cpp
@@ -1,99 +1,213 @@
+//
+// TCPServer.cpp
+//
+// $Id: //poco/1.4/Net/src/TCPServer.cpp#1 $
+//
+// Library: Net
+// Package: TCPServer
+// Module:  TCPServer
+//
+// Copyright (c) 2005-2006, Applied Informatics Software Engineering GmbH.
+// and Contributors.
+//
+// SPDX-License-Identifier:	BSL-1.0
+//
 
-#include "Poco/ByteOrder.h"
-#include "Poco/Net/IPAddress.h"
-#include  "Poco/Net/ServerSocket.h"
-#include  "Poco/Net/TCPServer.h"
-#include "Poco/Net/SocketAddress.h"
-#include "Poco/Net/TCPServerConnectionFactory.h"
+
+#include "Poco/Net/TCPServer.h"
+#include "Poco/Net/TCPServerDispatcher.h"
 #include "Poco/Net/TCPServerConnection.h"
-#include "Poco/Net/TCPServerParams.h"
-#include "Poco/AutoPtr.h"
+#include "Poco/Net/TCPServerConnectionFactory.h"
+#include "Poco/Timespan.h"
 #include "Poco/Exception.h"
-#include "Poco/Types.h"
-#include <iostream>
-#define WIN32_LEAN_AND_MEAN
-#include <winsock2.h>
-#include <stdio.h>
-#include <windows.h>
-#include "Poco/ByteOrder.h"
-#pragma comment(lib, "Ws2_32.lib")
-typedef Poco::AutoPtr <Poco::Net::TCPServerParams> Ptr;
-using namespace std;
+#include "Poco/ErrorHandler.h"
 
 
-//Create new connection for each Incoming Requests
-class createconnection : public Poco::Net::TCPServerConnection
-{
-public:
-	 createconnection(const Poco::Net::StreamSocket& s):Poco::Net::TCPServerConnection(s) {}; //functors
-	
-// Run() method inherited from the runnable class which is a entry point for each thread.
-	void run() 
+using Poco::ErrorHandler;
+
+
+namespace Poco {
+namespace Net {
+
+
+TCPServer::TCPServer(TCPServerConnectionFactory::Ptr pFactory, Poco::UInt16 portNumber, TCPServerParams::Ptr pParams):
+	_socket(ServerSocket(portNumber)),
+	_thread(threadName(_socket)),
+	_stopped(true)
+{	
+	Poco::ThreadPool& pool = Poco::ThreadPool::defaultPool();
+	if (pParams)
 	{
-		cout << "New connection from: " << socket().peerAddress().host().toString() << endl << flush;
-		bool active = true;
-		Poco::Timespan timeOut(100, 0);
-		unsigned char Buffer[1000];
-		while (active) {
-			if (socket().poll(timeOut, Poco::Net::Socket::SELECT_READ) == false) {
-				cout << "No string Recieved till Timeout span . Client Active status :"<< active  << flush;
-			}
-			else {
-				cout << " Client Message : "<< flush;
-				int recBytes = -1;
-
-				try {
-					recBytes = socket().receiveBytes(Buffer, sizeof(Buffer));
-				}
-				catch (Poco::Exception& exception) {
-					//Handle network errors.
-					cerr << "Network error: " << exception.displayText() << endl;
-					active= false;
-				}
+		int toAdd = pParams->getMaxThreads() - pool.capacity();
+		if (toAdd > 0) pool.addCapacity(toAdd);
+	}
+	_pDispatcher = new TCPServerDispatcher(pFactory, pool, pParams);
+	
+}
 
 
-				if (recBytes == 0) {
-					cout << "Client closes connection!" << endl << flush;
-					active = false;
+TCPServer::TCPServer(TCPServerConnectionFactory::Ptr pFactory, const ServerSocket& socket, TCPServerParams::Ptr pParams):
+	_socket(socket),
+	_thread(threadName(socket)),
+	_stopped(true)
+{
+	Poco::ThreadPool& pool = Poco::ThreadPool::defaultPool();
+	if (pParams)
+	{
+		int toAdd = pParams->getMaxThreads() - pool.capacity();
+		if (toAdd > 0) pool.addCapacity(toAdd);
+	}
+	_pDispatcher = new TCPServerDispatcher(pFactory, pool, pParams);
+}
+
+
+TCPServer::TCPServer(TCPServerConnectionFactory::Ptr pFactory, Poco::ThreadPool& threadPool, const ServerSocket& socket, TCPServerParams::Ptr pParams):
+	_socket(socket),
+	_pDispatcher(new TCPServerDispatcher(pFactory, threadPool, pParams)),
+	_thread(threadName(socket)),
+	_stopped(true)
+{
+}
+
+
+TCPServer::~TCPServer()
+{
+	try
+	{
+		stop();
+		_pDispatcher->release();
+	}
+	catch (...)
+	{
+		poco_unexpected();
+	}
+}
+
+
+const TCPServerParams& TCPServer::params() const
+{
+	return _pDispatcher->params();
+}
+
+
+void TCPServer::start()
+{
+	poco_assert (_stopped);
+
+	_stopped = false;
+	_thread.start(*this);
+}
+
+	
+void TCPServer::stop()
+{
+	if (!_stopped)
+	{
+		_stopped = true;
+		_thread.join();
+		_pDispatcher->stop();
+	}
+}
+
+
+void TCPServer::run()
+{
+	while (!_stopped)
+	{
+		Poco::Timespan timeout(250000);
+		try
+		{
+			if (_socket.poll(timeout, Socket::SELECT_READ))
+			{
+				try
+				{
+					StreamSocket ss = _socket.acceptConnection();
+					// enabe nodelay per default: OSX really needs that
+					ss.setNoDelay(true);
+					_pDispatcher->enqueue(ss);
 				}
-				else {
-					cout << "Receiving recBytes: " << recBytes << endl << flush;
-					cout <<"String Recieved : " <<Buffer << endl;
+				catch (Poco::Exception& exc)
+				{
+					ErrorHandler::handle(exc);
+				}
+				catch (std::exception& exc)
+				{
+					ErrorHandler::handle(exc);
+				}
+				catch (...)
+				{
+					ErrorHandler::handle();
 				}
 			}
 		}
-		cout << "Connection finished!" << endl << flush;
+		catch (Poco::Exception& exc)
+		{
+			ErrorHandler::handle(exc);
+			// possibly a resource issue since poll() failed;
+			// give some time to recover before trying again
+			Poco::Thread::sleep(50); 
+		}
 	}
-};
-	
-
-
-
-
-
-int main(int argc, char** argv)
-{   //Setting Server IP address
-
-	Poco::UInt32 ip = inet_addr("127.0.0.1");
-	Poco::Net::IPAddress ip1(&ip, sizeof(ip));
-	Poco::Net::SocketAddress ss(ip1, 21);
-	Poco::Net::ServerSocket s (ss,20);
-	Ptr p = new Poco::Net::TCPServerParams();
-
-	//Setting Server Parameters This will allow scaling .
-
-	p->setMaxThreads(64);
-	p->setMaxQueued(64);
-	p->setThreadIdleTime(50);
-
-	//Setting TcpServer constructor
-
-	Poco::Net::TCPServer t(new Poco::Net::TCPServerConnectionFactoryImpl<createconnection>(),s,p);
-
-	//Starting TCP Server
-	t.start();
-	cout << "Server Started" << endl;
-	cout << "Ready To Accept the connections" << endl;
-	while (1);
-	
 }
+
+
+int TCPServer::currentThreads() const
+{
+	return _pDispatcher->currentThreads();
+}
+
+
+int TCPServer::maxThreads() const
+{
+	return _pDispatcher->maxThreads();
+}
+
+	
+int TCPServer::totalConnections() const
+{
+	return _pDispatcher->totalConnections();
+}
+
+
+int TCPServer::currentConnections() const
+{
+	return _pDispatcher->currentConnections();
+}
+
+
+int TCPServer::maxConcurrentConnections() const
+{
+	return _pDispatcher->maxConcurrentConnections();
+}
+
+	
+int TCPServer::queuedConnections() const
+{
+	return _pDispatcher->queuedConnections();
+}
+
+
+int TCPServer::refusedConnections() const
+{
+	return _pDispatcher->refusedConnections();
+}
+
+
+std::string TCPServer::threadName(const ServerSocket& socket)
+{
+#if _WIN32_WCE == 0x0800
+	// Workaround for WEC2013: only the first call to getsockname()
+	// succeeds. To mitigate the impact of this bug, do not call
+	// socket.address(), which calls getsockname(), here.
+	std::string name("TCPServer");
+	#pragma message("Using WEC2013 getsockname() workaround in TCPServer::threadName(). Remove when no longer needed.")
+#else
+	std::string name("TCPServer: ");
+	name.append(socket.address().toString());
+#endif
+	return name;
+
+}
+
+
+} } // namespace Poco::Net
